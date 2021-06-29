4f5ac84520296b0df6933a0645a913c2509607d5
Avbey <azovsky777@gmail.com>
2021-06-29 16:52:06 +0300

    Fixed tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,13 +1,14 @@
 import pytest
 
 from parser.etl import ResumeETL
-from parser.config import TEST_DATA
+from config import TEST_DATA
 
 paths = [str(i) for i in TEST_DATA.rglob("*.docx")]
 
 
 @pytest.mark.parametrize("file_path", paths)
 def test_parser(file_path):
-    etl = ResumeETL(file_path=file_path)
-    resume = etl.process()
-    assert resume
+    with open(TEST_DATA / file_path, "rb") as f:
+        etl = ResumeETL(file=f)
+        resume = etl.get_resume()
+        assert resume
