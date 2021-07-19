b9128d61b6f9dc2bac1db2486f7c479cbddade15
Avbey <azovsky777@gmail.com>
2021-07-19 12:49:58 +0300

    Chat_id now obtained from the request Errors/Exceptions passed to bot

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 PROJECT_ROOT = pathlib.Path(__file__).parent
 TEST_DATA = PROJECT_ROOT / "tests" / "data"
 TG_TOKEN = os.getenv("TG_TOKEN")
-TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 NOTION_PAGE_ID = os.getenv("NOTION_PAGE_ID")
 
--- server/api.py
+++ server/api.py
@@ -7,16 +7,16 @@ from fastapi import FastAPI
 from notion_client import AsyncClient
 
 from parser.etl import ResumeETL
-from config import TG_TOKEN, TG_CHAT_ID, NOTION_TOKEN, NOTION_PAGE_ID
+from config import TG_TOKEN, NOTION_TOKEN, NOTION_PAGE_ID
 
 app = FastAPI()
 notion = AsyncClient(auth=NOTION_TOKEN)
 
 
-async def send_tg_message(message: str):
+async def send_tg_message(message: str, chat_id: int):
     API_URL = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
     async with httpx.AsyncClient() as client:
-        await client.post(API_URL, json={"chat_id": TG_CHAT_ID, "text": message, "parse_mode": "Markdown"})
+        await client.post(API_URL, json={"chat_id": chat_id, "text": message, "parse_mode": "Markdown"})
 
 
 async def get_file(url: str) -> bytes:
@@ -26,12 +26,16 @@ async def get_file(url: str) -> bytes:
 
 
 @app.get("/")
-async def convert(url: Optional[str]):
+async def convert(url: Optional[str], chat_id: Optional[int]):
     resp = await get_file(url)
     extractor = ResumeETL(file=io.BytesIO(resp))
-    resume = extractor.to_notion()
-    notion_resp = await notion.pages.create(parent={"page_id": NOTION_PAGE_ID}, **resume)
-    await send_tg_message(notion_resp["url"])
+    try:
+        resume = extractor.to_notion()
+        notion_resp = await notion.pages.create(parent={"page_id": NOTION_PAGE_ID}, **resume)
+        api_resp = notion_resp.get("url", "Notion Error")
+    except Exception as e:
+        api_resp = str(e)
+    await send_tg_message(api_resp, chat_id)
 
 
 if __name__ == "__main__":
