43d027a10ebc31996525801a3119150db7973755
ernest <azovsky777@gmail.com>
2021-09-21 19:04:57 +0300

    Cap

--- parse.py
+++ parse.py
@@ -8,8 +8,7 @@ from config import TEST_DATA, NOTION_TOKEN, NOTION_PAGE_ID
 notion = Client(auth=NOTION_TOKEN)
 
 
-# <file name>
-with open(TEST_DATA / "Ryabukha Artem.docx", "rb") as f:
+with open(TEST_DATA / "<file name>", "rb") as f:
     extractor = ResumeETL(file=f)
     resume = extractor.to_notion()
     # print(json.dumps(resume, indent=2, default=str, ensure_ascii=False))
