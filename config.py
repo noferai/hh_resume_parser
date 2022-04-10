0b8f37ca619fcf4a87ad437c341b1eaca2a65dad
ernest <azovsky777@gmail.com>
2022-04-11 01:52:03 +0300

    Added Sentry sdk

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
-import os
 import logging
+import os
 import pathlib
 
+import sentry_sdk
+
 logging.basicConfig(format="%(asctime)s %(levelname)-5s %(message)s", level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
-logger = logging.getLogger(__name__)
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 TEST_DATA = PROJECT_ROOT / "tests" / "data"
 TG_TOKEN = os.getenv("TG_TOKEN")
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 NOTION_PAGE_ID = os.getenv("NOTION_PAGE_ID")
+SENTRY_DSN = os.getenv("SENTRY_DSN")
 
 
-class LanguageException(Exception):
+class LanguageError(Exception):
     pass
+
+
+if SENTRY_DSN:
+    sentry_sdk.init(SENTRY_DSN, traces_sample_rate=1.0)
